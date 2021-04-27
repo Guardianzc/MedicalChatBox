be13eaa2e0eadf171544148e2daf793e98c8a1fc
yaoqianyuan <19210980044@fudan.edu.cn>
2021-04-28 00:01:17 +0800

    Update utils.py

--- Track1_Medical Dialogue Understanding/Task2_Symptom Attribute Recognition/MLC-NEAR/utils.py
+++ Track1_Medical Dialogue Understanding/Task2_Symptom Attribute Recognition/MLC-NEAR/utils.py
@@ -43,7 +43,7 @@ class BERTClass(torch.nn.Module):
         self.l3 = torch.nn.Linear(768, 987)
 
     def forward(self, ids, mask, token_type_ids):
-        _, output_1 = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids)#, return_dict=False)
+        _, output_1 = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids, return_dict=False)
         output_2 = self.l2(output_1)
         output = self.l3(output_2)
         return output
